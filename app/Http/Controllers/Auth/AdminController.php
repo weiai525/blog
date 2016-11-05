aaba33b32623ead91304dba688b745165310c17e
weiai525 <weiai525@163.com>
2016-11-05 22:04:20 +0800

    完善管理登陆操作

--- app/Http/Controllers/Auth/AdminController.php
+++ app/Http/Controllers/Auth/AdminController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Auth\AdminUser;
 use Auth;
-use App\Models\Auth\User;
-use Exception;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Request;
+use Illuminate\Http\Request;
 use Validator;
 
 class AdminController extends Controller
@@ -42,15 +41,20 @@ class AdminController extends Controller
     {
         //$this->middleware($this->guestMiddleware(), ['except' => 'logout']);
     }
-     public function getLogin()
+    public function getLogin()
     {
-        return tpl('auth.login');
+        return tpl('auth.login-admin');
     }
     public function postLogin(Request $request)
     {
-        $data = Request::only('email', 'password');
-        if (Auth::attempt($data, (boolean) Request::get('remember-me'))) {
-            return redirect()->intended('/');
+        $this->validate($request, [
+            'captcha' => 'required|captcha',
+            'email' => 'required|email|max:255|exists:admin_users,email',
+            'password' => 'required|min:6',
+        ]);
+        $data = $request->only('email', 'password');
+        if (Auth::guard('admin')->attempt($data, (boolean) $request->input('remember-me'))) {
+            return $this->success(['redirect_url' => $request->input('redirect_url', config('app.url'))]);
         }
         return $this->error(500201);
     }
@@ -60,20 +64,19 @@ class AdminController extends Controller
     }
     public function postRegister()
     {
-        return ;
         $validator = Validator::make(Request::all(), [
-            'captchas' => 'required|captcha',
+            'captcha' => 'required|captcha',
             'name' => 'required|max:255|min:5|unique:users',
             'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|min:6|confirmed',
             'password_confirmation' => 'required|min:6',
         ]);
         if ($validator->fails()) {
-            return $this->error(500201,$validator->errors()->first());
+            return $this->error(500201, $validator->errors()->first());
         }
         $user = $this->create(Request::all());
-        if (Auth::guard('web')->loginUsingId($user->id)) {
-            return $this->success(['redirect'=>'']);
+        if (Auth::guard('admin')->loginUsingId($user->id)) {
+            return $this->success(['redirect_url' => Request::input('redirect_url', config('app.url'))]);
         }
         return $this->error(500505);
     }
@@ -101,7 +104,7 @@ class AdminController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return AdminUser::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
