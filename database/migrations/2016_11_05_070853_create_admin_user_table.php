5d60d4bc3a2ea4a0161ac55b0f7b4c7bf3652356
weiai525 <weiai525@163.com>
2016-11-05 22:07:47 +0800

    database 后台管理员

---
+++ database/migrations/2016_11_05_070853_create_admin_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name',15);
+            $table->string('email',255)->unique();
+            $table->string('password',255);
+            $table->rememberToken();
+            $table->bigInteger('created_at');
+            $table->bigInteger('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('admin_user');
+    }
+}
